965fb9f66b3773eac2b730bcc29b533b20f46924
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-19 15:04:28 +0800

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toRemove) {
+
+    
+    let indexes = [];
+    for (const e of toRemove) {
+            let index = arr.indexOf(e);
+            if (index >= 0 && typeof arr[index] == typeof e) {
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
+    
+    // TOP solution:
+    // return arr.filter(e => !toRemove.includes(e)) // from TOP solution
 
 };
 
