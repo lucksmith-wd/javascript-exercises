b98c113c4087636c077f8550103ffa523bde3550
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-19 16:28:08 +0800

    Finish exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (from, to) {
+    if (from < 0 || to < 0 || !(typeof from === 'number') || !(typeof to === 'number')) {
+        return 'ERROR';
+    }
+    let sum = 0;
+    for (let i = Math.min(from, to); i <= Math.max(from, to); i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+sumAll(10, '10');
 
 // Do not edit below this line
 module.exports = sumAll;
