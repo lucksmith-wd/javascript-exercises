dd810b47bac1645213bf5e01f5614f98c980ce0a
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-19 10:20:53 +0800

    Finish exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let output = '';
+    for (let i = 0; i < num; i++) {
+        output += string;
+    }
+    return num < 0 ? 'ERROR' : output;
 };
 
 // Do not edit below this line
