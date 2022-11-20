dae275c1110f6f53344c35dbaa1d07ec71e9b411
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 12:21:39 +0800

    Finish exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+    let titles = [];
+    books.forEach(book => { titles.push(book.title) });
+    return titles;
 };
+
+// TOP solution
+// return array.map(book => book.title); 
 
 // Do not edit below this line
 module.exports = getTheTitles;
