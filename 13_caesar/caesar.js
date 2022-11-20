5574d83022968a2297d536fc9a9f3c1cba874913
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 19:35:27 +0800

    Finish exercise 13

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,20 @@
-const caesar = function() {
-
+const caesar = function (text, rot) {
+    textArr = text.split('');
+    for (let i = 0; i < textArr.length; i++) {
+        if (textArr[i].match(/[A-Za-z]/)) {
+            textArr[i] = changeLetter(textArr[i], rot);
+        }
+    }
+    return textArr.join('');
 };
+
+const changeLetter = (letter, rot) => {
+    if (rot < 0) rot = 26 + rot % 26; // A shift of -n = shift of 26-n
+    let oldCahrCode = letter.charCodeAt(0);
+    let newCharCode = (oldCahrCode - (oldCahrCode <= 90 ? 65 : 97) + rot) % 26 + (oldCahrCode <= 90 ? 65 : 97);
+    return String.fromCharCode(newCharCode);
+}
+console.log(caesar('a', -2354));
 
 // Do not edit below this line
 module.exports = caesar;
