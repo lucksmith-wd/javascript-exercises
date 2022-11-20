9e9bdadddefdea48e2da905858d74fc765642d72
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 15:53:21 +0800

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let currentYear = new Date().getFullYear();
+    let copyArr = [...people];
+    return copyArr.sort((a, b) => (((b.yearOfDeath || currentYear) - b.yearOfBirth)
+                                 - ((a.yearOfDeath || currentYear) - a.yearOfBirth)))[0];
 };
 
 // Do not edit below this line
