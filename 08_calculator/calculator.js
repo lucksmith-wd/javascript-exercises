ab20752fd9a0f7f4b5cc642a46e27fc56bf2251f
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 10:27:06 +0800

    Refine solutions for exercise 07

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,44 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (nums) {
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function (nums) {
+  let product = 1;
+  for (const num of nums) {
+    product *= num;
+  }
+  return nums.length ? product : 0;
 
+  // TOP solution
+  // return nums.length ? nums.reduce((a, b) => a * b) : 0;
 };
 
-const power = function() {
-	
+const power = function (num, exponent) {
+  return Math.pow(num, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let factorial = n--;
+  while (n > 1) {
+    factorial *= n--;
+  }
+  return factorial;
 };
+
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
