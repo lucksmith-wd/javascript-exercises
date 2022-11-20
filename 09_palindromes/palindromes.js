a37f4d65825b3102c3492b3f00866d139e73fea8
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 11:08:49 +0800

    Finish exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    let cleanText = text.toLowerCase().replace(/[^a-z]/g, "").split(' ').join('');
+    return cleanText.split('').reverse().join('') == cleanText;
 };
+
+palindromes("");
 
 // Do not edit below this line
 module.exports = palindromes;
