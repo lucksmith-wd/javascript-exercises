c3f28c427f745010402702a4f0536fcc3d3741df
lucksmith-wd <116928478+lucksmith-wd@users.noreply.github.com>
2022-11-20 11:36:20 +0800

    Finish exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+    if (n < 1) return 'OOPS';
+    let prev = 0;
+    let current = 1; //value of first fibonacci number
+    for (let i = 1; i < n; i++) {
+        let temp = current;
+        current += prev;
+        prev = temp;
+    }
+    return current;
 };
+
+console.log(fibonacci("8"));
 
 // Do not edit below this line
 module.exports = fibonacci;
